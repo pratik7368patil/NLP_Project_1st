3bbb3d16ac6821763a712c4c1320d3efad9623ae
Pratik Patil <pratik7368patil@gmail.com>
2019-11-04 23:48:23 +0530

    Added tokenization using nltk

--- data_processing_using_nltk.py
+++ data_processing_using_nltk.py
@@ -7,3 +7,13 @@ print(len(dist))
 # List of dist words
 vocab1 = list(dist.keys())
 print(vocab1[:10])
+
+# form tokens with nltk
+text12 = 'Recall from your high school grammar. that part-of-speech are these. verb classes like nouns, and verbs, and adjectives.'
+token = nltk.word_tokenize(text12)
+print(token)
+print(len(token))
+
+sen = nltk.sent_tokenize(text12)
+print(len(sen))
+print(sen)
